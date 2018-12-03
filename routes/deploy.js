db6d4c74f1b1f573b6d6c803ef32931c30be2699
Femi Nefa <feminefa@gmail.com>
2018-12-03 18:10:43 -0500

    first commit

---
+++ routes/deploy.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var router = express.Router();
+require('dotenv').config();
+const { exec } = require('child_process');
+router.post('/', function(req, res) {
+    //console.log(' deploy ', req.body);
+    const payload  = req.body;
+    if (payload.push
+        && payload.push.changes
+        && payload.push.changes[0]
+        && payload.push.changes[0].new
+      ) {
+        // run the deploy shell script
+        const resp = res;
+        exec(process.env[`${payload.push.changes[0].new.name}`], (err, stdout, stderr) => {
+            if (err) {
+                resp.send( err);
+                console.log(err);
+                return;
+            }
+            resp.send( `${stdout}` );
+        });
+        // res.send( payload.push.changes[0].new );
+        return;
+    }
+    res.send('Invalid request');
+
+});
+
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
